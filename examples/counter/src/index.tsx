7dbcd3768c9cf0d4d0ac47807173decdb2a1513c
ryym <ryym.64@gmail.com>
2019-04-12 08:15:54 +0900

    Create counter example (v2)

---
+++ examples/counter/src/index.tsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {createStore, applyMiddleware} from 'redux';
+import {
+  action,
+  effect,
+  defineReducer,
+  on,
+  redyMiddleware,
+  Dispatch,
+  Thunk,
+  connect,
+  Provider,
+} from 'redy';
+
+type State = {count: number};
+
+const Increment = (n: number) => action(n);
+
+const Decrement = (n: number) =>
+  action(n).effect(async () => {
+    console.log('DECREMENT!!!', n);
+  });
+
+const IncrementAsync = ({n, ms}: {n: number; ms: number}) =>
+  effect<Thunk<State>>(async dispatch => {
+    setTimeout(() => dispatch(Increment, n), ms);
+  });
+
+const reducer = defineReducer({count: 0}, [
+  on(Increment, ({count}, n) => ({count: count + n})),
+  on(Decrement, ({count}, n) => ({count: count - n})),
+]);
+
+const store = createStore(reducer, applyMiddleware(redyMiddleware()));
+
+type Props = {
+  count: number;
+  dispatch: Dispatch;
+};
+
+const Counter = function(props: Props) {
+  return (
+    <div>
+      <h1>count: {props.count}</h1>
+      <button onClick={() => props.dispatch(Increment, 3)}>Increment 3</button>
+      <button onClick={() => props.dispatch(Decrement, 2)}>Decrement 2</button>
+      <button onClick={() => props.dispatch(IncrementAsync, {n: 10, ms: 800})}>
+        Increment Async
+      </button>
+    </div>
+  );
+};
+
+const ConnectedCounter = connect((state: State) => state)(Counter);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <ConnectedCounter />
+  </Provider>,
+  document.getElementById('root'),
+);
